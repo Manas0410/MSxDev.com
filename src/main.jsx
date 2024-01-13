ceadea8e80753f482b060837ea71e26db4d7a3c2
manas0410 <manasshrivastava0410@gmail.com>
2024-01-13 12:22:03 -0800

    completely responsive funct services  page

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import Home from "./Home.jsx";
 // import "./index.css";
 import Service from "./ServicesComp/Services.jsx";
 import ContactPage from "./ContactUsComponent/ContactPage.jsx";
+import ServicePage from "./ServicesComp/page/ServicePage.jsx";
 
 const router = createBrowserRouter([
   {
@@ -21,8 +22,8 @@ const router = createBrowserRouter([
     element: <Home />,
   },
   {
-    path: "/test",
-    element: <Service />,
+    path: "/services",
+    element: <ServicePage />,
   },
   {
     path: "/contact",
