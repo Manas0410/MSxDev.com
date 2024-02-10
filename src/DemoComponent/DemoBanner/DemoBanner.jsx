c87cc1fb9bee9606c9319d15ca1ca93ace16b14c
manas0410 <manasshrivastava0410@gmail.com>
2024-02-10 23:47:02 +0530

    features added

--- src/DemoComponent/DemoBanner/DemoBanner.jsx
+++ src/DemoComponent/DemoBanner/DemoBanner.jsx
@@ -1,64 +1,108 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./styles/demoBanner.css";
 
+const IMAGE_MAP = [
+  { id: 1, img: "./portfolio.png" },
+  { id: 2, img: "./insta.png" },
+  { id: 3, img: "./Facebook_icon.svg.png" },
+  { id: 4, img: "./LinkedIn.png" },
+  { id: 5, img: "./portfolio.png" },
+];
+
+const SLIDES = [
+  { id: 1, src: "https://unbxd.com/integrations/search/" },
+  { id: 2, src: "https://manas-srivastava-portfolio.netlify.app/" },
+  { id: 3, src: "https://www.example.com/3" },
+  { id: 4, src: "https://www.example.com/3" },
+  { id: 5, src: "https://www.example.com/3" },
+];
+
 const DemoBanner = () => {
+  const [activeSlideIndex, setActiveSlideIndex] = useState(1);
+
   useEffect(() => {
     // No need to initialize Slick here as we will be using react-slick
     // We can handle slide changes using state and props in React
   }, []);
 
   const handleSlideClick = (slideIndex) => {
-    // This function will handle slide changes
-    // Slide index starts from 0, so we need to subtract 1
-    // We can set activeSlideIndex state to trigger re-rendering and change the slide
-    // However, we will keep it simple for demonstration
-    // alert("Go to slide " + (slideIndex + 1));
+    // Set the activeSlideIndex when a slide is clicked in the second slider
+    setActiveSlideIndex(slideIndex);
   };
 
-  return (
-    <div className="main">
-      <Slider
-        className="slider slider-for"
-        slidesToShow={1}
-        slidesToScroll={1}
-        arrows={false}
-        fade
-        asNavFor=".slider-nav"
-      >
-        {[1, 2, 3, 4, 5].map((index) => (
-          <div key={index}>
-            <h3 className="htClass">{index}</h3>
-          </div>
-        ))}
-      </Slider>
-      <Slider
-        className="slider slider-nav"
-        slidesToShow={3}
-        slidesToScroll={1}
-        asNavFor=".slider-for"
-        dots
-        focusOnSelect
-      >
-        {[1, 2, 3, 4, 5].map((index) => (
-          <div key={index}>
-            <h3 className="htClass">{index}</h3>
-          </div>
-        ))}
-      </Slider>
-      <div className="action">
-        <a href="#" onClick={() => handleSlideClick(2)}>
-          Go to slide 3
-        </a>
-        <a href="#" onClick={() => handleSlideClick(3)}>
-          Go to slide 4
-        </a>
-        <a href="#" onClick={() => handleSlideClick(4)}>
-          Go to slide 5
-        </a>
+  const handleAfterChange = (currentSlide) => {
+    // Set the activeSlideIndex after a slide change in the second slider
+    setActiveSlideIndex(currentSlide);
+  };
+
+  const settings1 = {
+    dots: false,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    className: "",
+
+    beforeChange: (current, next) => setActiveSlideIndex(next),
+  };
+
+  const settings2 = {
+    className: "slider slider-nav",
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: ".slider-for",
+    dots: true,
+    focusOnSelect: true,
+    afterChange: handleAfterChange,
+  };
+
+  let FigmaSlides = [];
+
+  FigmaSlides = IMAGE_MAP.map((slide, index) => {
+    let slideCss = `Slide`;
+    console.log("activeSlideIndex", activeSlideIndex);
+    console.log("slide.id", slide.id);
+    if (activeSlideIndex == slide.id) {
+      slideCss += " selected-Slide";
+    }
+    return (
+      <div key={slide.id}>
+        <iframe
+          frameborder="0"
+          title={`Slide ${slide.id}`}
+          src={SLIDES[index].src}
+          className={slideCss}
+        ></iframe>
       </div>
+    );
+  });
+
+  return (
+    <div className="main first-">
+      <div className="img-card-slider">
+        <Slider {...settings1}>{FigmaSlides}</Slider>
+      </div>
+
+      <br />
+      <br />
+      <Slider {...settings2}>
+        {IMAGE_MAP.map((item) => (
+          <div key={item.id}>
+            <img
+              src={item.img}
+              alt={`Slide ${item.id}`}
+              className={`slider-img ${
+                activeSlideIndex === item.id - 1 ? "selected" : ""
+              }`}
+              onClick={() => handleSlideClick(item.id - 1)}
+            />
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 };
